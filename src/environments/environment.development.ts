2eb909205d7ae6f98ef8e00295f3df84dc0fb7a1
Rafael Backx <rafael@sentigrate.com>
2024-07-12 13:48:59 +0200

    configured cognito userpools on angular

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,6 @@ export const environment : any = {
     REGION:'eu-west-1',
     AUTHENTICATOR_API:"https://authenticator.eu-west-1.sealution.sentigrate.com",
     BACKEND_API:"https://frontend.eu-west-1.sealution.sentigrate.com",
-    USERPOOL_ID: 'eu-west-1_rzC6YU8jJ',
-    USERPOOL_CLIENT_ID: '5ccqlt7ttqfqc7o46k0s6511rg'
+    USERPOOL_ID: 'eu-central-1_2YQbTBDDx',
+    USERPOOL_CLIENT_ID: '100pcrhle8panq9v6r5vencsi9'
 };
--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,13 @@ import { AppComponent } from './app/app.component';
 import { Amplify } from 'aws-amplify';
 import { environment } from './environments/environment';
 
-const awsconfig = {
-  "verion": "1",
-  "auth": {
-    "user_pool_id": environment.USERPOOL_ID,
-    "user_pool_client_id": environment.USERPOOL_CLIENT_ID,
-    "aws_region": environment.REGION
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: environment.USERPOOL_ID,
+      userPoolClientId: environment.USERPOOL_CLIENT_ID,
+    }
   }
-}
-
-Amplify.configure(awsconfig);
+})
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
