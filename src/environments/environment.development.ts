59bf592738172c5a51060ca72e832a3883cdbf3a
Rafael Backx <rafael@sentigrate.com>
2024-10-22 12:15:14 +0200

    added trace targets as a variable

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,4 +4,5 @@ export const environment : any = {
     USERPOOL_ID: 'eu-west-1_XaxgjMLWQ',
     USERPOOL_CLIENT_ID: '53hse3vdh3fr1hb62ode8lo5m9',
     SENTRY_DSN: "https://5dd6a83fe15a04f20b8f3fbf3659d47d@o4508087436247040.ingest.de.sentry.io/4508160448790608",
+    SENTRY_TRACE_TARGETS: ["localhost"],
 };
