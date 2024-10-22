a578571f598864f76f53bf16937794ffad432925
Rafael Backx <rafael@sentigrate.com>
2024-10-22 11:10:22 +0200

    added production url's to tracePropagationRoutes

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ Sentry.init({
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/,/^https:\/\/dev.sealution.eu-west-1.sentigrate\.com/],
   // Session Replay
   replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
